e83313fec2f2586a5aa35189a3a5e3cd5ba34ba5
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-01-28 04:53:48 +0100

    Remove old legacy cpp stuff

--- legacyCPP/src/DisplayException.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by fxdapokalypse on 22.10.15.
-//
-
-#include "DisplayException.h"
-
-DisplayException::DisplayException(std::string message) : runtime_error(message) {
-}
-DisplayException::~DisplayException() {}
