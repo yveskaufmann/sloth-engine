43dd0c9d748cae226b8f9aeef492a409d036c5a3
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-03-12 02:03:44 +0100

    Add radian/degree converter methods

--- src/main/java/de/yvka/slothengine/math/MathUtils.java
+++ src/main/java/de/yvka/slothengine/math/MathUtils.java
@@ -4,6 +4,8 @@ public class MathUtils {
 
 	public static final float PI = (float)Math.PI;
 	public static final float PI_2 = PI / 2.0f;
+	public static final float PI_180 = PI / 180;
+	public static final float PI_DIV_180 = 180 / PI;
 
 	/**
 	 * Take a float number and clamp it between min
@@ -16,5 +18,13 @@ public class MathUtils {
 	 */
 	public static float clamp(float number, float min, float max) {
 		return (number < min) ? min : (number > max) ? max : number;
+	}
+
+	public static float toRadians(float degrees) {
+		return degrees * PI_180;
+	}
+
+	public static float toDegrees(float radians) {
+		return radians * PI_DIV_180;
 	}
 }
